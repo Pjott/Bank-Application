bd7e219b176185a4b14b50c6e2e2b38d566ec243
Piotr Bala <piotr.baa@gmail.com>
2017-08-13 17:11:06 +0200

    Added signup view and update HomeController for signup

--- src/main/java/com/bankapp/controller/HomeController.java
+++ src/main/java/com/bankapp/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.bankapp.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bankapp.domain.User;
 
 @Controller
 public class HomeController {
@@ -14,5 +22,20 @@ public class HomeController {
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/singup", method = RequestMethod.GET)
+	public String singup(Model model) {
+		User user = new User();
+		
+		model.addAttribute("user", user);
+		
+		return "singup";
+	}
+	
+	@RequestMapping(value = "/singup", method = RequestMethod.POST)
+	public void singupPost(@ModelAttribute("user") User user, Model model) {
+		
+
 	}
 }
